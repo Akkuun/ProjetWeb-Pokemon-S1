95a3e439d3aa0cd8f74a1783c7d0e37c1e0efe49
Enzo <killerbackofheart@gmail.com>
2021-12-30 20:25:20 +0100

    Ajout de l'introduction dans index.php et mise en ordre du menu

--- src/index.php
+++ src/index.php
@@ -17,6 +17,23 @@
     <div id="--">
         <?php include('res/txt/-.txt'); ?>
     </div>
+    <div class="article">
+        <div class="text">
+            <h3>
+                Bienvenue !
+            </h3>
+            <p>
+                Bonjour et bienvenu sur notre site web, notre équipe est constitué de : <br>Mathis, Eliott, Cécile ainsi que moi-même Enzo. <br> Et je vous laisse en apprendre plus sur nous dans la section dédiée de notre site web.<br><br>
+
+                Nous avions à traiter de la « question » ou controverse socio technique de : <br><br><b>Les jeux de cartes à collectionner font-ils émerger un nouveau marché de l’art via internet ?</b><br><br>
+
+                Afin de répondre à cette controverse nous allons traiter dans un premier temps de la <b>définition</b> et des <b>caractéristiques</b> d’un « <b>Marché de l’art</b> », deuxièmement nous afficherons les similarités de ce marché de l’art avec le <b>marché des cartes</b>, plus spécifiquement des cartes pokemon.
+                et dans une troisième partie, avant de conclure nous <b>élargirons le marché des cartes</b> pokemon à <b>d’autres exemple de bulles spéculative</b> tel que la crypto monnaie.<br>
+
+
+            </p>
+        </div>
+    </div>
     <div style="height: 521px"></div>
 </main>
 
