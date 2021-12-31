1781d0503725bd9df621dca82ece6b335d8d2b6a
RusterX16 <EliottRuster@gmail.com>
2021-12-31 17:25:09 +0100

    Fixing orthography mistakes

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-C
-src/blockchain.html,1\e\1e2d75a7b1977a10ea89234ea38f497da35fb21f
--- src/index.php
+++ src/index.php
@@ -23,14 +23,22 @@
                 Bienvenue !
             </h3>
             <p>
-                Bonjour et bienvenu sur notre site web, notre équipe est constitué de : <br>Mathis, Eliott, Cécile ainsi que moi-même Enzo. <br> Et je vous laisse en apprendre plus sur nous dans la section dédiée de notre site web.<br><br>
-
-                Nous avions à traiter de la « question » ou controverse socio technique de : <br><br><b>Les jeux de cartes à collectionner font-ils émerger un nouveau marché de l’art via internet ?</b><br><br>
-
-                Afin de répondre à cette controverse nous allons traiter dans un premier temps de la <b>définition</b> et des <b>caractéristiques</b> d’un « <b>Marché de l’art</b> », deuxièmement nous afficherons les similarités de ce marché de l’art avec le <b>marché des cartes</b>, plus spécifiquement des cartes pokemon.
-                et dans une troisième partie, avant de conclure nous <b>élargirons le marché des cartes</b> pokemon à <b>d’autres exemple de bulles spéculative</b> tel que la crypto monnaie.<br>
-
-
+                Bonjour et bienvenu sur notre site web, notre équipe est constituée de : <br>Mathis, Eliott, Cécile
+                ainsi que moi-même Enzo.
+                <br>
+                Et je vous laisse en apprendre plus sur nous dans la section dédiée de notre site web.
+                <br><br>
+                Nous avions à traiter de la « question » ou controverse sociotechnique de : <br><br>
+                <strong>Les jeux de cartes à collectionner font-ils émerger un nouveau marché de l’art via internet
+                    ?</strong>
+                <br><br>
+                Afin de répondre à cette controverse nous allons traiter dans un premier temps de
+                la<strong>définition</strong>et des <strong>caractéristiques</strong> d’un « <strong>Marché de
+                    l’art</strong> », deuxièmement nous afficherons les similarités de ce marché de l’art avec le
+                <strong>marché des cartes</strong>, plus spécifiquement des cartes pokemon. Et dans une troisième
+                partie, avant de conclure nous <strong>élargirons le marché des cartes</strong>pokemon à <strong>d’autre
+                    exemple de bulles spéculative</strong> tel que la crypto monnaie.
+                <br>
             </p>
         </div>
     </div>
